e597e748e6efda71748bcfb647c6061d380779e5
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2026-09-12 11:02:33 -0400

    Clean up Osprey job and BIDS table checks

--- utilities/RunOspreyJob.m
+++ utilities/RunOspreyJob.m
@@ -1,6 +1,6 @@
 function [MRSCont] = RunOspreyJob(jobFilePath)
 %% [MRSCont] = RunOspreyJob(jobFilePath)
-%   This function creates performs a full Osprey analysis on the supplied 
+%   This function creates performs a full Osprey analysis on the supplied
 %   job file. This is the foundation for the compiled version of Osprey.
 %
 %   USAGE:
@@ -25,36 +25,35 @@ function [MRSCont] = RunOspreyJob(jobFilePath)
 %   HISTORY:
 %       2022-05-17: First version of the code.
 
-    MRSCont = OspreyJob(jobFilePath,0,'11');
+MRSCont = OspreyJob(jobFilePath,0,'11');
 
-    MRSCont = OspreyLoad(MRSCont);
+MRSCont = OspreyLoad(MRSCont);
 
-    MRSCont = OspreyProcess(MRSCont);
+MRSCont = OspreyProcess(MRSCont);
 
-    MRSCont = OspreyFit(MRSCont);
+MRSCont = OspreyFit(MRSCont);
 
-    if ~isempty(MRSCont.files_nii)
-        MRSCont = OspreyCoreg(MRSCont);
-        if ~isempty(MRSCont.files_seg)
-            seg = [num2str(~isempty(MRSCont.files_seg)) num2str(exist(MRSCont.files_seg{1}{1},'file'))];
-        else
-            seg = '00';
-        end
-        switch seg
-            case '00' %Do normal segmentation (No files_seg)
-                MRSCont = OspreySeg(MRSCont);
-            case '11' %files_seg are defined and finished.
-                MRSCont = OspreySeg(MRSCont);
-            case '12' %files_seg are defined and finished.
-                MRSCont = OspreySeg(MRSCont);
-            otherwise % (10) files_seg are defined, but not finished yet. (Do nothing)
-        end
-        
+if ~isempty(MRSCont.files_nii)
+    MRSCont = OspreyCoreg(MRSCont);
+    if ~isempty(MRSCont.files_seg)
+        seg = [num2str(~isempty(MRSCont.files_seg)) num2str(exist(MRSCont.files_seg{1}{1},'file'))];
+    else
+        seg = '00';
     end
+    switch seg
+        case '00' %Do normal segmentation (No files_seg)
+            MRSCont = OspreySeg(MRSCont);
+        case '11' %files_seg are defined and finished.
+            MRSCont = OspreySeg(MRSCont);
+        case '12' %files_seg are defined and finished.
+            MRSCont = OspreySeg(MRSCont);
+        otherwise % (10) files_seg are defined, but not finished yet. (Do nothing)
+    end
+end
 
-    MRSCont = OspreyQuantify(MRSCont);
+MRSCont = OspreyQuantify(MRSCont);
 
-    MRSCont = OspreyOverview(MRSCont);
+MRSCont = OspreyOverview(MRSCont);
 
     for kk = 1 : MRSCont.nDatasets
         [MRSCont] = OspreyHTMLReport(MRSCont,kk);
